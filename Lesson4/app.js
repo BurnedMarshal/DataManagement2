887bd9c7910eaf7472f90818989e688864b3f6f6
Daniele Manni <dan.manni@gmail.com>
2016-10-26 20:23:30 +0200

    Added lesson 4 Express app structure

---
+++ Lesson4/app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+// Import base routes
+const routes = require('./routes/index');
+
+// Init express app
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+// Setup logger and body parser
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+// Setup static public folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'jade');
+
+// Setup base routes
+app.use('/', routes);
+
+// Catch 404 errors
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
