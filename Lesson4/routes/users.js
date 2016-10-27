aec4954c6b8f1f584a484a154049b438fcf045f9
Daniele Manni <dan.manni@gmail.com>
2016-10-27 12:30:39 +0200

    Handle error

--- Lesson4/routes/users.js
+++ Lesson4/routes/users.js
@@ -5,6 +5,7 @@ var User = require('../models/users');
 
 router.get('/', function(req, res, next) {
     User.find(function(err, users){
+      if (err) return res.status(500).json({error: err});
       res.json(users);
     });
 })
